9110ed15b4fbb6c841848aa58c58d34579e3c2a9
Nina Ruiz <nina.ruizandres@gmail.com>
2020-04-25 11:55:21 +0200

    Add route to rock paper scissors page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 //Components
 import { IndexPageComponent } from './pages/index-page/index-page.component';
+import { RockPaperScissorsPageComponent } from './pages/rock-paper-scissors-page/rock-paper-scissors-page.component';
 
 
 const routes: Routes = [
   {path: './index', component: IndexPageComponent},
+  {path: './rockpaperscissors', component: RockPaperScissorsPageComponent},
   //{ path: '**', component: PageNotFoundComponent } // Wildcard route for a 404 page
   { path: '',   redirectTo: './index', pathMatch: 'full' }
 ];
